f8a47c5955ff2502f6b3f2af8c67b2af50126c99
limelier <tudor.iacobescu.1@gmail.com>
2020-10-08 13:17:16 +0300

    Add 2 new functions to lab 3

--- lab1/ex3.py
+++ lab1/ex3.py
@@ -2,12 +2,32 @@ def count(source: str, substring: str) -> int:
     return source.count(substring)
 
 
+def count_overlapping_simple(source: str, substring: str) -> int:
+    occurrences: int = 0
+    size = len(substring)
+    # all_substrings = [source[i:i+size] for i in range(0, len(source) - size + 1)]
+    for i in range(0, len(source) - size + 1):
+        if source[i:i+size] == substring:
+            occurrences += 1
+    return occurrences
+
+
 def count_overlapping(source: str, substring: str) -> int:
-    occurences = 0
     size = len(substring)
     all_substrings = [source[i:i+size] for i in range(0, len(source) - size + 1)]
     return all_substrings.count(substring)
 
 
+def count_overlapping_2(source: str, substring: str) -> int:
+    occurrences: int = 0
+    pos = 0
+    while True:
+        pos = source.find(substring, pos) + 1
+        if pos == 0:
+            break
+        occurrences += 1
+    return occurrences
+
+
 if __name__ == '__main__':
-    print(count_overlapping("aaaaa", "aaa"))
+    print(count_overlapping_2("aaaaa", "aaa"))
